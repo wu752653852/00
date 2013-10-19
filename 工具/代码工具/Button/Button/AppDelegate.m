161c9d300317292ac4903ff9d82195e43522e74f
wwegos <wu752653852@sohu.com>
2013-10-19 12:25:25 +0800

    12:25

--- 工具/代码工具/Button/Button/AppDelegate.m
+++ 工具/代码工具/Button/Button/AppDelegate.m
@@ -9,7 +9,6 @@
 #import "AppDelegate.h"
  #import "ButtonView.h"
 
-wocaonidaye
 @implementation AppDelegate
 //@synthesize window;
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
