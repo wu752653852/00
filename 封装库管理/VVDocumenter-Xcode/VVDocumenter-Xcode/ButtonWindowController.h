4501d059d02a1c2f18968886b8430861ac906b4f
wwegos <wu752653852@sohu.com>
2013-10-21 05:04:55 +0800

    5:04

---
+++ 封装库管理/VVDocumenter-Xcode/VVDocumenter-Xcode/ButtonWindowController.h
@@ -0,0 +1,13 @@
+//
+//  ButtonWindowController.h
+//  fengzhuang
+//
+//  Created by wwegos on 13-10-21.
+//  Copyright (c) 2013年 wwegos. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ButtonWindowController : NSWindowController
+
+@end
